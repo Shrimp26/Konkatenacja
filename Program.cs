92a3a09a9771a52a1daee67d789d04849874f18b
julia <julia@LAPTOP-ILMLJSM->
2024-02-10 03:29:13 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+//Napisz program, który składa dowolne dwa łańcuchy tekstowe. Użytkownik podaje pierwszy i drugi łańcuch. Wyświetl wyniki tej operacji.
+
+class Konkatenacja
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Podaj piwerszy łańcuch tekstowy: ");
+        string pierwszyLancuch = Console.ReadLine();
+
+        Console.WriteLine("Podaj drugi łańcuch tekstowy: ");
+        string drugiLancuch = Console.ReadLine();
+
+        string poloczoneLancuchy = pierwszyLancuch + drugiLancuch;
+        Console.WriteLine($"Wynik połączenia podanych łańcuchów: {poloczoneLancuchy}.");
+    }
+}
